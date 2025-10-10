bca139209deaeca380305b404c458528fc346649
Rahul Mondal <rahul.mondal.dev@gmail.com>
2025-10-10 17:22:57 +0530

    Rename activity_main layout to activity_home_page

--- app/src/main/java/com/rahulmondal/weatherapp/ui/home/HomePageActivity.kt
+++ app/src/main/java/com/rahulmondal/weatherapp/ui/home/HomePageActivity.kt
@@ -1,7 +1,6 @@
 package com.rahulmondal.weatherapp.ui.home
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -18,7 +17,7 @@ class HomePageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_home_page)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
